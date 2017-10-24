9596cc2e03ab094be4a880ceef4bf8e1b75c3442
fulihao <fulihao@boco.com.cn>
2017-10-24 18:21:30 +0800

    2

--- test/src/test/Hello.java
+++ test/src/test/Hello.java
@@ -17,12 +17,12 @@ public class Hello {
 		System.out.println("baibai");
 		System.out.println("see you");
 		System.out.println("see you");
-		System.out.println("nihao");
-		System.out.println("baibai");
-		System.out.println("see you");
-		System.out.println("nihao");
-		System.out.println("baibai");
-		System.out.println("see you");
+		System.out.println("nio");
+		System.out.println("bbai");
+		System.out.println("seeou");
+		System.out.println("nio");
+		System.out.println("baii");
+		System.out.println("seeou");
 	}
 
 }
