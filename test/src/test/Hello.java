7677156df6a564bd888e1fa861582cd988c7b05e
fulihao <32888391+fulihao@users.noreply.github.com>
2017-10-27 15:41:02 +0800

    Update Hello.java

--- test/src/test/Hello.java
+++ test/src/test/Hello.java
@@ -4,25 +4,13 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("nihao");
-		System.out.println("baibai");
+		System.out.println("baibi");
+		System.out.println("see ou");
+		System.out.println("babai");
+		System.out.println("se you");
 		System.out.println("see you");
-		System.out.println("nihao");
-		System.out.println("baibai");
-		System.out.println("see you");
-		System.out.println("nihao");
-		System.out.println("baibai");
-		System.out.println("see you");
-		System.out.println("nihao");
-		System.out.println("baibai");
-		System.out.println("see you");
-		System.out.println("see you");
-		System.out.println("nio");
-		System.out.println("bbai");
-		System.out.println("seeou");
-		System.out.println("nioasdf");
-		System.out.println("baiiadsf");
-		System.out.println("seeouasdf");
+		System.out.println("baiadsf");
+		
 	}
 
 }
