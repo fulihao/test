56ce1303ccc9f14e4f3fee43039336798453d407
fulihao <fulihao@boco.com.cn>
2017-10-27 15:45:42 +0800

    hao

--- test/src/test/Hello.java
+++ test/src/test/Hello.java
@@ -4,25 +4,10 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("nihao");
-		System.out.println("baibai");
-		System.out.println("see you");
-		System.out.println("nihao");
-		System.out.println("baibai");
-		System.out.println("see you");
-		System.out.println("nihao");
-		System.out.println("baibai");
-		System.out.println("see you");
-		System.out.println("nihao");
-		System.out.println("baibai");
-		System.out.println("see you");
-		System.out.println("see you");
-		System.out.println("nio");
-		System.out.println("bbai");
-		System.out.println("seeou");
-		System.out.println("nioasdf");
-		System.out.println("baiiadsf");
-		System.out.println("seeouasdf");
+		System.out.println("niho");
+		System.out.println("baiai");
+		System.out.println("baiiasf");
+		System.out.println("seeousdf");
 	}
 
 }
